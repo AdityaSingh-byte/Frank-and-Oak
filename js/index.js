6e8ed9f6c2527b39385407a82622f27e088948e1
RSarvjyoti <sarvjyoti64@gmail.com>
2024-03-28 19:35:04 +0530

    Completed dropdown menu

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ function moveCarousel(direction) {
 }
 
 function startAutoSlide() {
-    timer = setInterval(() => moveCarousel('auto'), 5000); 
+    timer = setInterval(() => moveCarousel('auto'), 5000);
 }
 
 function stopAutoSlide() {
@@ -27,3 +27,68 @@ function stopAutoSlide() {
 
 window.onload = startAutoSlide;
 
+// start open dropdown
+
+let dropDown = document.getElementsByClassName("dropDown")[0];
+let women = document.getElementById("women");
+let men = document.getElementById("men");
+let closeTimeout;
+
+function openMenu() {
+    dropDown.classList.add("open-menu");
+}
+
+function closeMenu() {
+    clearTimeout(closeTimeout);
+    closeTimeout = setTimeout(function () {
+        dropDown.classList.remove("open-menu");
+    }, 200);
+}
+
+women.addEventListener("mouseover", openMenu);
+women.addEventListener("mouseleave", closeMenu);
+
+men.addEventListener("mouseover", openMenu);
+men.addEventListener("mouseleave", closeMenu);
+
+let sales = document.getElementById("sales");
+let salesDropDown = document.getElementsByClassName("salesDropDown")[0];
+
+let closeTime;
+
+function openMenus() {
+    salesDropDown.classList.add("open-menus");
+}
+
+function closeMenus() {
+    clearTimeout(closeTime);
+    closeTime = setTimeout(function () {
+        salesDropDown.classList.remove("open-menus");
+    }, 200);
+}
+
+sales.addEventListener("mouseover", openMenus);
+sales.addEventListener("mouseleave", closeMenus);
+
+
+let ourStory = document.getElementById("ourStory");
+let ourStoryDropDown = document.getElementsByClassName("ourStoryDropDown")[0];
+
+let closeTimer;
+
+function openMenubar() {
+    ourStoryDropDown.classList.add("open-menubar");
+}
+
+function closeMenubar() {
+    clearTimeout(closeTimer);
+    closeTimer = setTimeout(function () {
+        ourStoryDropDown.classList.remove("open-menubar");
+    }, 200);
+}
+
+ourStory.addEventListener("mouseover", openMenubar);
+ourStory.addEventListener("mouseleave", closeMenubar);
+
+
+// end open dropdown
